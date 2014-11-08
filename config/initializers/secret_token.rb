ef2f16176560e981474c2385c1f347bf4e810404
Jason Kotchoff <cornflakesuperstar@hotmail.com>
2014-11-08 04:20:03 -0800

    vanilla rails 3.2.18 app with no database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuRailsMicrosoftAccessMdbExample::Application.config.secret_token = '75cc83e4c295db675e1207f0187366b06cd90e43f07882ad9be4bcd35933f2e14d0d49c6ce015840e7bcdbb06306b66188a8f4211ee88044081d40700bc2e56b'
